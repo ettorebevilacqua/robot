a4e5e9d6872daaf0f830980594d487504f2af941
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2022-12-11 02:39:39 +0100

    improve compile perfomance

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,11 @@
-use std::env;
 
-use chrono::format::strftime;
-
-#[derive(Debug)]
-struct Config {
-    simulate: bool,
-    exchange: String,
-    pairs: String,
-    count: u64,
-}
 
 fn main() {
 
-    let config = Config::new();
+    let config = robot::get_config();
 
     println!("{:?}", config);
     // println!("In file {}", file_path);
 }
 
-impl Config{
-    fn new( )->Config{
-    let mut config = Config {
-        simulate: true,
-        exchange: String::from("binance"),
-        pairs: String::from("btc/usdt"),
-        count: 0,
-    };
 
-    let mut args = env::args()
-    .skip(1)   // skip program name
-    .peekable(); // allow looking forward one
-
-    match args.peek().map(|x| x.as_ref()) {
-        Some("--exchange") => {
-            args.next();
-            let val = args.next().ok_or("err").expect("");
-            config.exchange = val
-        }
-        Some("--pairs") => {
-            let val = args.next().ok_or("err").expect(""); // Skip the flag
-            config.pairs =val;
-        }
-        _ => {
-            println!("handle no option");
-        }
-    }
-    config
-    }
-}
