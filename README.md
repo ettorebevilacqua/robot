67e5dafd1cfd451e94cd1caff9f784f299f0eeaa
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2022-12-11 03:48:53 +0100

    multi report ready

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 https://endler.dev/2020/rust-compile-times/
+
+https://github.com/JamesMenetrey/rust-multipackages-template/
---
+++ client/src/main.rs
@@ -0,0 +1,11 @@
+
+
+fn main() {
+
+    let config = client::get_config(); // ::main();  //_get_config();
+
+    println!("{:?}", config);
+    // println!("In file {}", file_path);
+}
+
+
